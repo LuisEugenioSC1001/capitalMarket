252e25f6976450f0e9012995b5517d5b11415bda
LuisEugenioSC1001 <90073572+LuisEugenioSC1001@users.noreply.github.com>
2021-11-03 14:12:44 -0600

    Update Api.js

--- src/shared/utils/Api.js
+++ src/shared/utils/Api.js
@@ -1,2 +1,2 @@
-const apiBaseUrl ="http://ec2-3-140-30-87.us-east-2.compute.amazonaws.com:4000/"
+const apiBaseUrl ="http://ec2-3-140-30-87.us-east-2.compute.amazonaws.com:4000"
 export default apiBaseUrl;
